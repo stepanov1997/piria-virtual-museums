c9fbd17dc4c659ad8979ebd8e8666f0364deca2f
Kristijan Stepanov <kristijan.stepanov95@gmail.com>
2023-05-04 01:13:09 +0200

    Second commit

--- virtual-museum/src/main/kotlin/com/virtual/museum/VirtualMuseumApplication.kt
+++ virtual-museum/src/main/kotlin/com/virtual/museum/VirtualMuseumApplication.kt
@@ -1,5 +1,8 @@
 package com.virtual.museum
 
+import com.github.dockerjava.api.DockerClient
+import com.github.dockerjava.core.DefaultDockerClientConfig
+import com.github.dockerjava.core.DockerClientBuilder
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
@@ -7,5 +10,12 @@ import org.springframework.boot.runApplication
 class VirtualMuseumApplication
 
 fun main(args: Array<String>) {
+    Runtime.getRuntime().addShutdownHook(Thread{
+        val dockerConfig = DefaultDockerClientConfig.createDefaultConfigBuilder().build()
+        val dockerClient: DockerClient = DockerClientBuilder.getInstance(dockerConfig).build()
+        val removeContainerCmd = dockerClient.removeContainerCmd("mysql").withForce(true)
+        removeContainerCmd.exec()
+        println("MySQL kontejner je uklonjen.")
+    })
     runApplication<VirtualMuseumApplication>(*args)
 }
