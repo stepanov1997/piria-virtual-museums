dc4ed4ed9b36192827490714d0edbdddc95e3d76
Kristijan Stepanov <kristijan.stepanov95@gmail.com>
2023-06-28 01:00:29 +0200

    Added database support to Bank API.

--- virtual-bank/database/data.sql
+++ virtual-bank/database/data.sql
@@ -1,10 +1,32 @@
 PRAGMA default_cache_size = 0;
 PRAGMA cache_size = 0;
-create table main."BANK_TRANSACTION"
+CREATE TABLE main."CLIENT"
 (
-    id       integer not null
-        constraint id primary key autoincrement,
-    clientId integer not null,
-    amount   REAL default 0,
-    datetime TEXT    not null
+    id         INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    name       TEXT    NOT NULL,
+    surname    TEXT    NOT NULL,
+    cardNumber TEXT    NOT NULL,
+    cardType   TEXT    NOT NULL,
+    cardExpire TEXT    NOT NULL,
+    pin        TEXT    NOT NULL,
+    balance    REAL    DEFAULT 0.0
 );
+
+CREATE TABLE main."BANK_TRANSACTION"
+(
+    id       INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    clientId INTEGER NOT NULL REFERENCES CLIENT(id),
+    amount   REAL    DEFAULT 0,
+    datetime TEXT    NOT NULL
+);
+
+INSERT INTO main."CLIENT" (name, surname, cardNumber, cardType, cardExpire, pin, balance)
+VALUES ('John', 'Doe', '1234567812345678', 'VISA', '12/25', '1234', 1000),
+       ('Virtual', 'Museum', '8765432187654321', 'MASTERCARD', '08/24', '5678', 1000),
+       ('Michael', 'Johnson', '5555444433332222', 'AMERICAN EXPRESS', '06/26', '9876', 1000);
+
+INSERT INTO main."BANK_TRANSACTION" (clientId, amount, datetime)
+VALUES (1, 100.00, '2023-06-27 09:00:00'),
+       (1, 50.00, '2023-06-27 14:30:00'),
+       (2, 75.50, '2023-06-27 12:15:00'),
+       (3, 200.00, '2023-06-27 16:45:00');
--- virtual-bank/routes/index.js
+++ virtual-bank/routes/index.js
@@ -1,48 +1,172 @@
 const express = require('express');
-const crypto = require('crypto');
-const fs = require('fs');
 const router = express.Router();
-const { v4: uuidv4 } = require('uuid');
-const {sep} = require("path");
-const path = require("path");
 const jwt = require('jsonwebtoken');
+const db = require('../database/database')
+const {TokenExpiredError} = require("jsonwebtoken");
 
 const secretKey = 'my-secret-key';
 
-router.post('/authenticatePayment', async (req, res) => {
-  // Simulate payment processing logic
-  const { cardHolderFirstname, cardHolderSurname, cardNumber, cardType, cardExpiration } = req.body;
-
-  // Perform necessary validation and processing
-  if (cardHolderFirstname && cardHolderSurname && cardNumber && cardType && cardExpiration) {
-    const payload = { cardHolderFirstname, cardHolderSurname, cardNumber, cardType, cardExpiration }
-    const token = jwt.sign(payload, secretKey, { expiresIn: '1m' });
-
-    // Payment successful
-    res.status(200).json({
-      message: 'Payment authentication successful',
-      jwtPaymentToken: token
+function retrieveClientsFromDatabase() {
+    return new Promise((resolve, reject) => {
+        db.all("SELECT * FROM main.'CLIENT'", [], (err, rows) => {
+            if (err) {
+                console.error(err.message);
+                reject(err);
+                return;
+            }
+            resolve(rows);
+        });
     });
-  } else {
-    // Payment failed
-    res.status(400).json({ message: 'Payment authentication failed' });
-  }
+}
+
+router.post('/authenticatePayment', async (req, res) => {
+    // Simulate payment processing logic
+    const {cardHolderFirstname, cardHolderSurname, cardNumber, cardType, cardExpiration, pin} = req.body;
+
+    // Perform necessary validation and processing
+    if (cardHolderFirstname && cardHolderSurname && cardNumber && cardType && cardExpiration && pin) {
+        const payload = {cardHolderFirstname, cardHolderSurname, cardNumber, cardType, cardExpiration, pin}
+        const clients = await retrieveClientsFromDatabase()
+        if(!clients) {
+            res.status(400).json({message: 'Data is wrong.'});
+            return
+        }
+
+        const client = clients.find(e => {
+            return e.name === cardHolderFirstname
+                && e.surname === cardHolderSurname
+                && e.cardNumber === cardNumber
+                && e.cardType === cardType
+                && e.cardExpire === cardExpiration
+                && e.pin === pin
+        })
+        if (!!client) {
+            const token = jwt.sign({id:client.id}, secretKey, {expiresIn: '1m'});
+
+            // Payment successful
+            res.status(200).json({
+                message: 'Payment authentication successful',
+                jwtPaymentToken: token
+            });
+        } else {
+            res.status(400).json({message: 'Payment authentication failed'});
+        }
+    } else {
+        // Payment failed
+        res.status(400).json({message: 'Payment authentication failed'});
+    }
 });
+
+function sendAmount(senderAccountId, receiverAccountId, amount) {
+    return new Promise((resolve, reject) => {
+        db.serialize(() => {
+            db.run('BEGIN TRANSACTION');
+            db.get('SELECT balance FROM CLIENT WHERE id = ?', [senderAccountId], (err, senderAccount) => {
+                if (err) {
+                    console.error(err.message);
+                    db.run('ROLLBACK', () => {
+                        reject(new Error("Error while executing query"));
+                    });
+                    return;
+                }
+
+                if (!senderAccount) {
+                    console.error('Sender account not found');
+                    db.run('ROLLBACK', () => {
+                        reject(new Error("Sender account not found"));
+                    });
+                    return;
+                }
+
+                if (senderAccount.balance < amount) {
+                    console.error('Insufficient balance');
+                    db.run('ROLLBACK', () => {
+                        reject(new Error("Insufficient balance"));
+                    });
+                    return;
+                }
+
+                db.run('UPDATE CLIENT SET balance = balance - ? WHERE id = ?', [amount, senderAccountId], (err) => {
+                    if (err) {
+                        console.error(err.message);
+                        db.run('ROLLBACK', () => {
+                            reject(new Error(err.message));
+                        });
+                        return;
+                    }
+
+                    db.run('UPDATE CLIENT SET balance = balance + ? WHERE id = ?', [amount, receiverAccountId], (err) => {
+                        if (err) {
+                            console.error(err.message);
+                            db.run('ROLLBACK', () => {
+                                reject(new Error(err.message));
+                            });
+                            return;
+                        }
+
+                        db.run('COMMIT', (err) => {
+                            if (err) {
+                                console.error(err.message);
+                                db.run('ROLLBACK', () => {
+                                    reject(new Error(err.message));
+                                });
+                                return;
+                            }
+
+                            console.log(`Successfully sent ${amount} units from account ${senderAccountId} to account ${receiverAccountId}`);
+                            resolve();
+                        });
+                    });
+                });
+            });
+        });
+    });
+}
+
 
 router.post('/payment', async (req, res) => {
-  // Simulate payment processing logic
-  const { paymentToken, amount } = req.body;
+    // Simulate payment processing logic
+    const {receiverCardNumber, amount} = req.body;
 
-  try {
-    // Payment successful
-    const decoded = await jwt.verify(paymentToken, secretKey);
-    console.log('Decoded token:', decoded);
-    res.status(200).json({ message: 'Payment successful' });
-  } catch (error) {
-    // Payment failed
-    console.error('Invalid token:', error.message);
-    res.status(400).json({message: 'Payment failed'});
-  }
+    try {
+        // Payment successful
+        const paymentToken = exportTokenFromRequest(req);
+        const decoded = await jwt.verify(paymentToken, secretKey);
+        console.log('Decoded token:', decoded);
+
+        const clients = await retrieveClientsFromDatabase()
+        const receiverAccountId = clients.find(e => e.cardNumber === receiverCardNumber).id
+        if(!receiverAccountId) {
+            // Payment failed
+            res.status(400).json({message: 'Receiver card is not valid.'});
+        }
+        const errorMessage = await sendAmount(decoded.id, receiverAccountId, amount)
+        if(!errorMessage) {
+            res.status(200).json({message: 'Payment successful.'});
+            return
+        }
+        // Payment failed
+        res.status(400).json({message: errorMessage});
+    } catch (error) {
+        // Payment failed
+        if(error instanceof TokenExpiredError) {
+            res.status(400).json({message: 'Token expired.'});
+        }
+        console.error('Invalid token:', error.message);
+        res.status(400).json({message: 'Payment failed'});
+    }
 });
+
+function exportTokenFromRequest(req) {
+    const authorizationHeader = req.headers['authorization'];
+    if (!authorizationHeader) {
+        return undefined
+    }
+    const [authType, token] = authorizationHeader.split(' ');
+    if (authType.toLowerCase() !== 'bearer' || !token) {
+        throw new Error("Invalid token");
+    }
+    return token;
+}
 
 module.exports = router;
